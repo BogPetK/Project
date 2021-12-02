cc83dba10b21dd115ff81f60584d3ba162cb07e4
BogPetK <petrichenko.b.i20@gmail.com>
2021-12-02 12:27:11 +0300

    3.2

--- src/components/Auth/Login.vue
+++ src/components/Auth/Login.vue
@@ -13,12 +13,14 @@
 				name="email" 
 				label="Email" 
 					type="email">
+					<v-model="email">
 					</v-text-field>
 					<v-text-field
 					prepend-icon="mdi-lock" 
 					name="password" 
 					label="Password" 
 					type="password">
+					<v-model="password">
 					</v-text-field>
 					</v-form> 
 				</v-card-text>
@@ -35,6 +37,8 @@
 export default {
 	data () { 
 		return {
+			email: "",
+			password: ""
 		} 	
 	}
 } 
