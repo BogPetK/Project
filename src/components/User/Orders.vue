38d9a7d1206394a20f8e0cad18645ed0e818188a
BogPetK <petrichenko.b.i20@gmail.com>
2021-12-12 02:35:32 +0300

    6.2

--- src/components/User/Orders.vue
+++ src/components/User/Orders.vue
@@ -8,20 +8,23 @@
 	two-line
 	flat
     >		      
-		<v-list-item>
+		<v-list-item
+			v-for="order in orders"
+			:key="order.id"
+			>
 	<v-list-item-action>
 	<v-checkbox
-					:input-value="active"
+					:input-value="order.done"
 					color="primary"
 	></v-checkbox>
 	</v-list-item-action>
 
 				<v-list-item-content>
-				<v-list-item-title>Notifications</v-list-item-title>
-    <v-list-item-subtitle>Allow notifications</v-list-item-subtitle>
+				<v-list-item-title>{{ order.name }}</v-list-item-title>
+    <v-list-item-subtitle>{{ order.phone }}</v-list-item-subtitle>
     </v-list-item-content>	        
     <v-list-item-action>
-				<v-btn class="primary">Open</v-btn>
+				<v-btn class="primary" :to="'/ad/' + order.adId">Open</v-btn>
 </v-list-item-action>
     </v-list-item>
 		</v-list>
@@ -34,6 +37,15 @@
 export default {
 	data () { 
 		return {
+			orders: [
+			{
+				id:"123",
+				name:"Kostya",
+				phone:"+7(978)000-00-05",
+				adId:"1",
+				done:true
+			}
+			]
 		} 	
 	}
 } 
