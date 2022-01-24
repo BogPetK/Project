9a3c65dfc459a8c623ff106cc92ac7430fde87b8
BogPetK <petrichenko.b.i20@gmail.com>
2022-01-24 12:25:44 +0300

    16.3

--- src/App.vue
+++ src/App.vue
@@ -19,19 +19,28 @@
       <v-list dense>
         <v-list-item-group color="primary">
           <v-list-item
- v-for="link in links"
- :key="link.title"
- :to="link.url"
->
+            v-for="link in links"
+            :key="link.title"
+            :to="link.url">
+          <v-list-item-icon>
+            <v-icon>{{ link.icon }}</v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title>{{ link.title }}</v-list-item-title>
+          </v-list-item-content>
+    </v-list-item>
+    <v-list-item 
+    @click="onLogout"
+    v-if="isUserLoggedIn"
+    >
+    <v-list-item-icon>
+    <v-icon>mdi-exit-to-app</v-icon>
+    </v-list-item-icon>
+    <v-list-item-content>
+    <v-list-item-title>Logout</v-list-item-title>
+    </v-list-item-content>
+    </v-list-item>
 
-            <v-list-item-icon>
-              <v-icon>{{ link.icon }}</v-icon>
-            </v-list-item-icon>
-            <v-list-item-content>
-              <v-list-item-title>{{ link.title }}</v-list-item-title>
-            </v-list-item-content>
-      </v-list-item>
-      
 </v-list-item-group>
 
       </v-list>
@@ -51,6 +60,14 @@
       :key="link.title"
       :to="link.url" 
       text><v-icon left>{{ link.icon }}</v-icon>{{ link.title }}</v-btn>
+      <v-btn
+      text
+      @click="onLogout"
+      v-if="isUserLoggedIn"
+      >
+      <v-icon left>mdi-exit-to-app</v-icon>
+       Logout
+      </v-btn>
     </v-toolbar-items>
   </v-app-bar>
   <v-main>
@@ -109,8 +126,12 @@ links () {
   methods: {
     closeError () {
       this.$store.dispatch('clearError')
+    },
+    onLogout () {
+      this.$store.dispatch('logoutUser')
+      this.$router.push("/")
     }
   }
-};
+}
 </script>
 
